092821a32fdea1f0bfe0072839e1218a752838a0
Hope Zhang <403496063zhp@gmail.com>
2023-11-08 11:23:43 +0000

    Code is Prettier

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 let i = 1;
-while (i < 101){
-    let text = i
-    if(i%3 === 0){
-        text = "Fizz"
+while (i < 101) {
+  let text = i;
+  if (i % 3 === 0) {
+    text = "Fizz";
+  }
+  if (i % 5 === 0) {
+    if (text === i) {
+      text = "Buzz";
+    } else {
+      text += "Buzz";
     }
-    if(i%5 === 0){
-        if(text === i){
-            text = "Buzz"
-        }
-        else{
-            text += "Buzz"
-        }
-    }
-    console.log(text);
-    i++;
+  }
+  console.log(text);
+  i++;
 }
