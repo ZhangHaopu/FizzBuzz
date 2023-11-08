60cd43ca057666cc471e5d22ac7b68f62d2eb040
Hope Zhang <403496063zhp@gmail.com>
2023-11-08 11:11:42 +0000

    Part 1

---
+++ index.js
@@ -0,0 +1,17 @@
+let i = 1;
+while (i < 101){
+    let text = i
+    if(i%3 === 0){
+        text = "Fizz"
+    }
+    if(i%5 === 0){
+        if(text === i){
+            text = "Buzz"
+        }
+        else{
+            text += "Buzz"
+        }
+    }
+    console.log(text);
+    i++;
+}
