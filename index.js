3d2132658d493d9b4453355eab89056a30ef5905
Hope Zhang <403496063zhp@gmail.com>
2023-11-08 12:30:52 +0000

    Refactor Part2

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 let i = 1;
-while (i < 201) {
-  let text = "";
-  let text3 = "";
-  let text5 = "";
-  let text7 = "";
-  let text11 = "";
-  let text13 = "";
+while (i < 300) {
+  let texts = [];
+
   if (i % 3 === 0) {
-    text3 = "Fizz";
+    texts.push("Fizz");
   }
   if (i % 5 === 0) {
-    text5 = "Buzz";
+    texts.push("Buzz");
   }
   if (i % 7 === 0) {
-    text7 = "Bang";
+    texts.push("Bang");
   }
   if (i % 11 === 0) {
-    text11 = "Bong";
+    texts.push("Bong");
   }
   if (i % 13 === 0) {
-    text13 = "Fezz";
-  }
-  if (i % 17) {
-    if (i % 3 === 0) {
-      text = text3 + text13 + text5 + text7 + text11;
-    } else {
-      text = text13 + text3 + text5 + text7 + text11;
+    let len = texts.length;
+    let index = 0;
+    for (let j = 0; j < len; j++) {
+      index = j;
+      if (texts[j][0] === "B") {
+        break;
+      }
     }
-  } else {
-    if (i % 3 === 0) {
-      text = text11 + text7 + text5 + text13 + text3;
-    } else {
-      text = text11 + text7 + text5 + text3 + text13;
-    }
+    texts.splice(index, 0, "Fezz");
   }
+
+  if (i % 17 === 0) {
+    texts.reverse();
+  }
+  let text = texts.join("");
+
   if (text === "") {
     text = i;
   }
